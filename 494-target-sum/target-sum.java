1050040c38846a537cf7984ac0b29e6100468657
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-08-03 21:07:45 +0530

    Time: 90 ms (41.34%) | Memory: 47.3 MB (5.85%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    private int helper(int index,int[] nums,int target){
+    private int helper(int index,int[] nums,int target,HashMap<String,Integer>map){
         if(index==0){
             if(nums[0]==target&&nums[0]==target*-1){
                 return 2;
@@ -9,12 +9,24 @@ class Solution {
             }
             return 0;
         }
-        int l=nums[index]+helper(index-1,nums,target-nums[index]);
-        int r=-1*nums[index]+helper(index-1,nums,target+nums[index]);
+        String s=index+","+target;
+        if(map.containsKey(s)){
+            return map.get(s);
+        }
+        int l=0;
+        if(index>0){l=helper(index-1,nums,target-nums[index],map);}
+        int r=0;
+        if(index>0){r=helper(index-1,nums,target+nums[index],map);}
+        // dp[index][target]=l+r;
+        map.put(s,l+r);
         return l+r;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n=nums.length;
-        return helper(n-1,nums,target);
+        HashMap<String,Integer> map=new HashMap();
+        // for(int[] row:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        return helper(n-1,nums,target,map);
     }
 }
