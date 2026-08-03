da601f577bd539ec50d7e01a11c46d61a06ed460
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-08-03 11:56:01 +0530

    Time: 15 ms (82.28%) | Memory: 47 MB (30.10%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -20,13 +20,35 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        // for(int[] row:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        // int x=helper(n-1,coins,amount,dp);
+        // if(x>=(int)1e9){
+        //     return -1;
+        // }
+        for(int target=0;target<=amount;target++){
+            if(target%coins[0]==0){
+                dp[0][target]=target/coins[0];
+            }
+            else{
+                dp[0][target]=(int)1e9;
+            }
         }
-        int x=helper(n-1,coins,amount,dp);
-        if(x>=(int)1e9){
+        for(int i=1;i<n;i++){
+            for(int target=0;target<=amount;target++){
+                int l=0+dp[i-1][target];
+                int r=Integer.MAX_VALUE;
+                if(coins[i]<=target){
+                    r=1+dp[i][target-coins[i]];
+                }
+                dp[i][target]=Math.min(r,l);
+                // return dp[index][amount];
+            }
+        }
+        if(dp[n-1][amount]==(int)1e9){
             return -1;
         }
-        return x;
+        return dp[n-1][amount];
     }
 }
