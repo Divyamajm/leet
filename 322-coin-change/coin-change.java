350fe3ad41c22823f6d9d7b11f7d87a7a7a96009
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-08-03 13:10:51 +0530

    Time: 16 ms (68.41%) | Memory: 47 MB (25.04%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -20,6 +20,8 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
+        int[]prev=new int[amount+1];
+        int[]temp=new int[amount+1];
         // for(int[] row:dp){
         //     Arrays.fill(row,-1);
         // }
@@ -29,26 +31,26 @@ class Solution {
         // }
         for(int target=0;target<=amount;target++){
             if(target%coins[0]==0){
-                dp[0][target]=target/coins[0];
+                prev[target]=target/coins[0];
             }
             else{
-                dp[0][target]=(int)1e9;
+                prev[target]=(int)1e9;
             }
         }
         for(int i=1;i<n;i++){
             for(int target=0;target<=amount;target++){
-                int l=0+dp[i-1][target];
+                int l=0+prev[target];
                 int r=Integer.MAX_VALUE;
                 if(coins[i]<=target){
-                    r=1+dp[i][target-coins[i]];
+                    r=1+temp[target-coins[i]];
                 }
-                dp[i][target]=Math.min(r,l);
+                temp[target]=Math.min(r,l);
                 // return dp[index][amount];
-            }
+            }prev=temp;
         }
-        if(dp[n-1][amount]==(int)1e9){
+        if(prev[amount]==(int)1e9){
             return -1;
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 }
