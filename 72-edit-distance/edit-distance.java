53ad90c4961123e395c00a2bae65994aada2489d
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-08-06 19:32:38 +0530

    Time: 5 ms (66.73%) | Memory: 47.1 MB (72.19%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,31 +1,50 @@
 class Solution {
-    private int helper(int ind1,int ind2,String word1,String word2,int[][]dp){
-        if(ind1==0){
-            return ind2;
-        }
-        if(ind2==0){
-            return ind1;
-        }
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-        }
-        if(word1.charAt(ind1-1)==word2.charAt(ind2-1)){
-            // return 
-            dp[ind1][ind2]=helper(ind1-1,ind2-1,word1,word2,dp);
-            return dp[ind1][ind2];
-        }
-        else{
-            dp[ind1][ind2]=1+Math.min(helper(ind1-1,ind2,word1,word2,dp),Math.min(helper(ind1,ind2-1,word1,word2,dp),helper(ind1-1,ind2-1,word1,word2,dp)));
-            return dp[ind1][ind2];
-        }
-    }
+    // private int helper(int ind1,int ind2,String word1,String word2,int[][]dp){
+    //     if(ind1==0){
+    //         return ind2;
+    //     }
+    //     if(ind2==0){
+    //         return ind1;
+    //     }
+    //     if(dp[ind1][ind2]!=-1){
+    //         return dp[ind1][ind2];
+    //     }
+    //     if(word1.charAt(ind1-1)==word2.charAt(ind2-1)){
+    //         // return 
+    //         dp[ind1][ind2]=helper(ind1-1,ind2-1,word1,word2,dp);
+    //         return dp[ind1][ind2];
+    //     }
+    //     else{
+    //         dp[ind1][ind2]=1+Math.min(helper(ind1-1,ind2,word1,word2,dp),Math.min(helper(ind1,ind2-1,word1,word2,dp),helper(ind1-1,ind2-1,word1,word2,dp)));
+    //         return dp[ind1][ind2];
+    //     }
+    // }
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
         int[][] dp=new int[n+1][m+1];
-        for(int[]row: dp){
-            Arrays.fill(row,-1);
+        // for(int[]row: dp){
+        //     Arrays.fill(row,-1);
+        // }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
         }
-        return helper(n,m,word1,word2,dp);
+        for(int i=1;i<=m;i++){
+            dp[0][i]=i;
+        }
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(word1.charAt(ind1-1)==word2.charAt(ind2-1)){
+                    // return 
+                    dp[ind1][ind2]=dp[ind1-1][ind2-1];
+                    // return dp[ind1][ind2];
+                }
+                else{
+                    dp[ind1][ind2]=1+Math.min(dp[ind1-1][ind2],Math.min(dp[ind1][ind2-1],dp[ind1-1][ind2-1]));
+                    // return dp[ind1][ind2];
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
