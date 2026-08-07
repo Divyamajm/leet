ed98f805e555daaef821d63cd46951dce0e8582f
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-08-07 16:00:32 +0530

    Time: 89 ms (25.77%) | Memory: 131.7 MB (24.38%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,42 +1,44 @@
 class Solution {
-    private int helper(int ind,int buy,int[] prices,int cap,int[][][] dp){
-        if(cap==0){
-            return 0;
-        }
-        if(ind==prices.length){
-            return 0;
-        }
-        if(dp[ind][buy][cap]!=-1){
-            return dp[ind][buy][cap];
-        }
-        int profit=0;
-        if(buy==1){
-            profit=Math.max(-prices[ind]+helper(ind+1,0,prices,cap,dp),helper(ind+1,1,prices,cap,dp));
-        }
-        else{
-            profit=Math.max(prices[ind]+helper(ind+1,1,prices,cap-1,dp),helper(ind+1,0,prices,cap,dp));
-        }
-        dp[ind][buy][cap]=profit;
-        return dp[ind][buy][cap];
-    }
+    // private int helper(int ind,int buy,int[] prices,int cap,int[][][] dp){
+    //     if(cap==0){
+    //         return 0;
+    //     }
+    //     if(ind==prices.length){
+    //         return 0;
+    //     }
+    //     if(dp[ind][buy][cap]!=-1){
+    //         return dp[ind][buy][cap];
+    //     }
+    //     int profit=0;
+    //     if(buy==1){
+    //         profit=Math.max(-prices[ind]+helper(ind+1,0,prices,cap,dp),helper(ind+1,1,prices,cap,dp));
+    //     }
+    //     else{
+    //         profit=Math.max(prices[ind]+helper(ind+1,1,prices,cap-1,dp),helper(ind+1,0,prices,cap,dp));
+    //     }
+    //     dp[ind][buy][cap]=profit;
+    //     return dp[ind][buy][cap];
+    // }
     public int maxProfit(int[] prices) {
         int n=prices.length;
         int[][][]dp=new int[n+1][2][3];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<2;j++){
+        //         for(int k=0;k<3;k++){
+        //             dp[i][j][k]=-1;
+        //         }
+        //     }
+        // }
+        int[][]prev=new int[2][3];
+        int[][]temp=new int[2][3];
         for(int i=0;i<n;i++){
             for(int j=0;j<2;j++){
-                for(int k=0;k<3;k++){
-                    dp[i][j][k]=-1;
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<2;j++){
-                dp[i][j][0]=0;
+                prev[j][0]=0;
             }
         }
         for(int j=0;j<2;j++){
             for(int k=0;k<3;k++){
-                dp[0][j][k]=0;
+                prev[j][k]=0;
             }
         }
         for(int ind=n-1;ind>=0;ind--){
@@ -44,16 +46,17 @@ class Solution {
                 for(int cap=1;cap<3;cap++){
                     int profit=0;
                 if(buy==1){
-                    profit=Math.max(-prices[ind]+dp[ind+1][0][cap],dp[ind+1][1][cap]);
+                    profit=Math.max(-prices[ind]+prev[0][cap],prev[1][cap]);
                 }
                 else{
-                    profit=Math.max(prices[ind]+dp[ind+1][1][cap-1],dp[ind+1][0][cap]);
+                    profit=Math.max(prices[ind]+prev[1][cap-1],prev[0][cap]);
                 }
-                dp[ind][buy][cap]=profit;
+                temp[buy][cap]=profit;
                 // return dp[ind][buy][cap];
                 }
             }
+            prev=temp.clone();
         }
-        return dp[0][1][2];
+        return prev[1][2];
     }
 }
