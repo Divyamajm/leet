c4259e4ee101b7cc95b01ada30425e966440c354
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-08-08 15:33:25 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (65.31%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,21 +1,5 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n<0){
-            x=1/x;
-            n=-n;
-        }
-        return helper(x,n);
-    }
-    public static double helper(double x,int n){
-        if(n==0){
-            return 1;
-        }
-        double half=helper(x,n/2);
-        if(n%2==0){
-            return half*half;
-        }
-        else{
-            return half*half*x;
-        }
+        return Math.pow(x,n);
     }
 }
