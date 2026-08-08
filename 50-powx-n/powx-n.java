dc6b52836b224ecd7d46f2036d5cfb98e6ef1532
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-08-08 15:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (90.23%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,27 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        double power=n;
+        if(n<0){
+            x=1/x;
+            power=-power;
+        }
+        double ans=1.0;
+        while(power>0){
+            if(power%2==1){
+                ans*=x;
+                power--;
+            }
+            else{
+                x=x*x;
+                power=power/2;
+            }
+        }
+        // if(apowers>Ipowerteger.MAX_VALUE){
+        //     returpower 0;
+        // }
+        // if(apowers<Ipowerteger.MIpower_VALUE){
+        //     returpower 0;
+        // }
+        return ans;
     }
 }
