f28d68c4bb6cc94abcbe1f60ea42850e15f5d99a
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-08-14 15:43:28 +0530

    Time: 1 ms (99.89%) | Memory: 55.7 MB (46.86%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,12 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
+        int count=0;
+        int element=-1;
         int n=nums.length;
         for(int i=0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        PriorityQueue<Integer>q=new PriorityQueue<>((a,b)->Integer.compare(map.get(b),map.get(a)));
-        q.addAll(map.keySet());
-        return q.peek();
+            if(count==0){
+                element=nums[i];
+            }
+            if(nums[i]==element){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }return element;
     }
 }
