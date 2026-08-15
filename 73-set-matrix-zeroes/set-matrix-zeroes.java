4018b3a935a89480a413bc4cf23d27cbcad34b1a
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-08-15 09:47:49 +0530

    Time: 1 ms (97.48%) | Memory: 47.5 MB (62.77%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,35 +2,36 @@ class Solution {
     private void row(int row,int[][] matrix){
         int m=matrix[0].length;
         for(int i=0;i<m;i++){
-            if(matrix[row][i]!=0){
-                matrix[row][i]=Integer.MIN_VALUE+88;
-            }
+            matrix[row][i]=0;
         }
     }
     private void col(int col,int[][] matrix){
         int n=matrix.length;
         for(int i=0;i<n;i++){
-            if(matrix[i][col]!=0){
-                matrix[i][col]=Integer.MIN_VALUE+88;
-            }
+            matrix[i][col]=0;
         }
     }
     public void setZeroes(int[][] matrix) {
         int n=matrix.length;
         int m=matrix[0].length;
+        int[] row=new int[n];
+        int[] col=new int[m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    row(i,matrix);
-                    col(j,matrix);
+                    row[i]=-1;
+                    col[j]=-1;
                 }
             }
         }
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==Integer.MIN_VALUE+88){
-                    matrix[i][j]=0;
-                }
+            if(row[i]==-1){
+                row(i,matrix);
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(col[i]==-1){
+                col(i,matrix);
             }
         }
     }
