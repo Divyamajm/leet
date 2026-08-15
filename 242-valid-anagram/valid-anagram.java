353617257645e50cc2b821cb8ecc8c603b78895e
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-08-15 16:32:19 +0530

    Time: 4 ms (83.01%) | Memory: 44.3 MB (75.01%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] array=new int[26];
+        Arrays.fill(array,0);
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            array[s.charAt(i)-'a']++;
+            array[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(array[i]!=0){
+                return false;
+            }
+        }return true;
+    }
+}
