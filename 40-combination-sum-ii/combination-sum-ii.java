6306ea213133943d83c1585119c3a45433a4cf0e
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-09-08 14:39:21 +0530

    Time: 7 ms (35.35%) | Memory: 48.2 MB (9.10%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    private void helper(int index,int[] candidates,int target,Set<List<Integer>> result,List<Integer> current){
+        if(candidates.length==index){
+            if(target==0){
+                result.add(new ArrayList<>(current));
+            }return;
+        }
+        if(candidates[index]<=target){
+            current.add(candidates[index]);
+            helper(index+1,candidates,target-candidates[index],result,current);
+            current.remove(current.size()-1);
+        }
+        while(index+1<candidates.length && candidates[index]==candidates[index+1]){
+            index++;
+        }
+        helper(index+1,candidates,target,result,current);
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Set<List<Integer>> result=new HashSet();
+        Arrays.sort(candidates);
+        helper(0,candidates,target,result,new ArrayList<>());
+        List<List<Integer>> result1=new ArrayList<>(result);
+        return result1;
+    }
+}
