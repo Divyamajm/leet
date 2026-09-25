2ac9ee4842051fd25eb35e01568292c947151db3
Divyamajm <156990299+Divyamajm@users.noreply.github.com>
2026-09-26 00:29:42 +0530

    Time: 2 ms (99.40%) | Memory: 47.1 MB (59.21%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,3 +1,10 @@
+// 1. Two Sum
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n=nums.length;
